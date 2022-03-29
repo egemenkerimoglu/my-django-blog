f93f393ae6e3fe692025df12ec8637fda1200d16
egemenkerimgolu <egemen@elkomyazilim.com>
2022-03-29 23:33:43 +0300

    Site için HTML dosyasını değiştirdim.

---
+++ blog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+
+"""
+Gördüğünüz üzere, ana URL'e post_list adında bir view atıyoruz.
+ Django URL resolver ful URL'in başındaki domain adını 
+ (yani, http://127.0.0.1:8000 /) göz ardı eder, böylece bu URL
+  kalıbı (pattern) boş bir string ile eşleşecek. Bu kalıp, 
+  Django'ya eğer siteye biri 'http://127.0.0.1:8000/' adresinden 
+  gelirse gitmesi gereken yerin views.post_list olduğunu söylüyor.
+
+Son kısım name='post_list', görünümü (view) tanımlamak için kullanılan
+URL'in adıdır. Bu view'un adı ile aynı olabilir ama tamamen farklı bir
+ şey de olabilir. Bundan sonra projede isimlendirilmiş URL'leri 
+ kullanıyor olacağız, bu yüzden uygulamadaki her URL'i isimlendirmek 
+ önemli. Aynı zamanda URL isimlerini eşsiz ve kolay hatırlanabilir 
+ şekilde seçmeliyiz.
+"""
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+"""
+Burada post_list isimli bir fonksiyon (def) yarattık, 
+bu fonksiyon girdi olarak request (isteği) alıp,
+blog/post_list.html template'ini işleyecek olan render
+fonksiyonunu döndürüyor.
+"""
+def post_list(request):
+    return render(request,'blog/post_list.html',{})
